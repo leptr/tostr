3ac76d3c7002937960ce9d1411be3b8206360aec
leptr <petar.mijailovic2000@gmail.com>
2019-07-20 21:33:53 +0200

    Tostr

---
+++ tostr.js
@@ -0,0 +1,44 @@
+const SUCCESS = "success";
+const ERROR = "error";
+const WARN = "warn";
+
+const TOP = "top";
+const BOTTOM = "bottom";
+const TOP_LEFT = "top-left";
+const TOP_RIGHT = "top-right";
+const BOTTOM_LEFT = "bottom-left";
+const BOTTOM_RIGHT = "bottom-right";
+
+const ToastMessage = function(message, type, position, lifespan) {
+  this.message = message;
+  this.type = type;
+  this.position = position;
+  this.lifespan = lifespan;
+  this.element = undefined;
+
+  this.show();
+};
+
+ToastMessage.prototype.show = function() {
+  this.element = document.createElement("div");
+  this.element.classList.add("toast-message");
+  this.element.classList.add(this.position);
+  this.element.classList.add(this.type);
+  this.element.textContent = this.message;
+
+  document.body.appendChild(this.element);
+
+  setTimeout(() => {
+    this.element.classList.add("shown");
+    setTimeout(() => {
+      this.element.classList.remove("shown");
+      setTimeout(() => {
+        this.element.remove();
+      }, 500);
+    }, this.lifespan);
+  }, 50);
+};
+
+function showToast(message, type, position, lifespan) {
+  new ToastMessage(message, type, position, lifespan);
+}
